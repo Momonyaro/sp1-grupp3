4af87bc96770a615fb5136876eec66c919fa3f07
Teodor Schulman <teodor.schulman@gmail.com>
2020-02-05 11:51:15 +0100

    lol

--- Assets/Greetings.cs
+++ Assets/Greetings.cs
@@ -7,7 +7,7 @@ public class Greetings : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Please god don't do this shit.
     }
 
     // Update is called once per frame
