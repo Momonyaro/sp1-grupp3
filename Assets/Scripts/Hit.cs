4828075d182342ee9388515960ec754c137f22a2
b19stiha <b19stiha@users.noreply.github.com>
2020-02-11 05:36:00 +0100

    s

--- Assets/Scripts/Hit.cs
+++ Assets/Scripts/Hit.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Hit : MonoBehaviour
 {
+    [SerializeField] float knockbackPower = 500f;
     BoatMovementV01 boat;
     bool croc = false;
     string player = "Player";
+    bool countingTime = false;
+    float counter = 0f;
 
     void Start()
     {
@@ -15,7 +19,7 @@ public class Hit : MonoBehaviour
 
     void Update()
     {
-        
+
     }
 
     public void StoneHit(Collider2D collision, GameObject stone) //knockback lite, sten dör
@@ -23,8 +27,9 @@ public class Hit : MonoBehaviour
         if(collision.tag == player)
         {
             Destroy(stone);
+
+            //boat.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -knockbackPower));
             //animation sten går sönder
-            
             Debug.Log("Hit by stone");
         }
     }
