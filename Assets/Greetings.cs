3b107239cf74f7a61ca85ca0d9e7a2266fd5edef
b19stiha <b19stiha@users.noreply.github.com>
2020-02-13 10:40:43 +0100

    death

---
+++ Assets/Greetings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Greetings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Please god don't do this shit.
+        Debug.Log(":)");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
