aacd54ce484757bfbe01792668a579a11cde7e35
b19stiha <b19stiha@users.noreply.github.com>
2020-02-14 11:54:53 +0100

    Merge branch 'stina-prog' into dev

---
+++ Assets/Scripts/Dangers/wirlpool.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wirlpool : MonoBehaviour
+{
+    BoatMovementV01 boat;
+    bool whirl = false;
+    public float intoWhirlSpeed = 8f;
+    bool activated = false;
+
+    void Start()
+    {
+        boat = FindObjectOfType<BoatMovementV01>();
+    }
+
+    void Update()
+    {
+        if (activated)
+        {
+            MoveBoat();
+        }
+    }
+
+    public void MoveBoat()
+    {
+        boat.transform.position = Vector3.MoveTowards(boat.transform.position, transform.position, intoWhirlSpeed * Time.deltaTime);
+        if (transform.position == boat.transform.position)
+        {
+            boat.StopBoatSwitchBool();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        activated = true;
+    }
+}
--- Assets/Scripts/Movement/BoatMovementV01.cs
+++ Assets/Scripts/Movement/BoatMovementV01.cs
@@ -9,11 +9,12 @@ public class BoatMovementV01 : MonoBehaviour
     public float breakSpeed = 1.0f;
     public float rowSpeed = 8.0f;
     public float rowPower = 500.0f;
-    [Tooltip("Value not used (yet at least)")]
+    public float intoWhirlSpeed = 10f;
     [SerializeField] float currentSpeedY = 0;
     bool knockback = false;
     bool stunned = false;
     bool shield = false;
+    bool stopBoat = false;
 
     public static int maxHealth = 3;
     [SerializeField] int currentHealth;
@@ -39,12 +40,10 @@ public class BoatMovementV01 : MonoBehaviour
             if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) && stunned == false)
             {
                 autoSpeed = breakSpeed;
-                //Debug.Log("S i pressed");
             }
             else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) && stunned == false)
             {
                 autoSpeed = rowSpeed;
-                //Debug.Log("W is pressed");
             }
             else
             {
@@ -52,6 +51,7 @@ public class BoatMovementV01 : MonoBehaviour
             }
 
             Vector2 position = transform.position;
+
             if(stunned)
             {
                 position.x = position.x + tiltSpeed * -horizontal * Time.unscaledDeltaTime;
@@ -70,6 +70,15 @@ public class BoatMovementV01 : MonoBehaviour
         {
             autoSpeed = 0;
         }
+        if (stopBoat)
+        {
+            autoSpeed = 0;
+        }
+    }
+
+    public void StopBoatSwitchBool()
+    {
+        stopBoat = !stopBoat;
     }
 
     public void KnockbackBoolSwitch()
