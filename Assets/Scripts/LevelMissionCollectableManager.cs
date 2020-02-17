9384373e428fae52e1e3793243d17b3a1590a3be
Teodor Schulman <teodor.schulman@gmail.com>
2020-02-17 12:25:38 +0100

    Testing...

---
+++ Assets/Scripts/LevelMissionCollectableManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelMissionCollectableManager : MonoBehaviour
+{
+    public GameObject collectable1;
+    public Image thing1;
+    public GameObject collectable2;
+    public GameObject collectable3;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
