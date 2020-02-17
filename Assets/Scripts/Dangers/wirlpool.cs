782acfac54afd980cbd2f9d04ac0234cd542cb1f
b19stiha <b19stiha@users.noreply.github.com>
2020-02-17 09:19:43 +0100

    Whirlpool 1.0 done

--- Assets/Scripts/Dangers/wirlpool.cs
+++ Assets/Scripts/Dangers/wirlpool.cs
@@ -1,12 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class wirlpool : MonoBehaviour
 {
     BoatMovementV01 boat;
-    bool whirl = false;
     public float intoWhirlSpeed = 8f;
+    public int clicksForRelease = 10;
+    public int clicksClicked = 0;
+    [Tooltip("Seconds until the click counter reset")]
+    public float secLimit = 3f;
+    [Tooltip("How many times the clock resets before auto-release")]
+    public int autoRelease = 4;
+    [Tooltip("How many times the clock has restarted")]
+    public int autoReleaseCounter = 0;
+    float counter = 0f;
     bool activated = false;
 
     void Start()
@@ -19,15 +28,42 @@ public class wirlpool : MonoBehaviour
         if (activated)
         {
             MoveBoat();
+            CountClicks();
         }
     }
 
     public void MoveBoat()
     {
         boat.transform.position = Vector3.MoveTowards(boat.transform.position, transform.position, intoWhirlSpeed * Time.deltaTime);
-        if (transform.position == boat.transform.position)
+
+        if (transform.position == boat.transform.position && clicksClicked < clicksForRelease)
         {
-            boat.StopBoatSwitchBool();
+            boat.transform.position = transform.position;
+        }
+        if(clicksClicked >= clicksForRelease)
+        {
+            activated = false;
+        }
+    }
+
+    private void CountClicks()
+    {
+        counter += Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            clicksClicked++;
+            Debug.Log("clicks clicked: " + clicksClicked);
+        }
+        if(counter >= secLimit)
+        {
+            clicksClicked = 0;
+            counter = 0f;
+            autoReleaseCounter++;
+            Debug.Log("autoReleaseCounter: " + autoReleaseCounter);
+        }
+        if(autoRelease <= autoReleaseCounter)
+        {
+            activated = false;
         }
     }
 
