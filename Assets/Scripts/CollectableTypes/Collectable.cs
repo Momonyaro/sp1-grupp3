f768abf1564f5142824f3cfc717571e5a8a08319
Teodor Schulman <teodor.schulman@gmail.com>
2020-02-18 12:23:48 +0100

    Collectables currently done

---
+++ Assets/Scripts/CollectableTypes/Collectable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    public AudioSource collectSound;
+    public int collectableScore = 1;
+    [Tooltip("Use a negative number to rotate the other way")]
+    public float rotationSpeed = 0;
+
+    private void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, 1) * rotationSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (collectSound != null)
+            {
+                collectSound.Play();
+            }
+            TextManager.score += collectableScore;
+            Destroy(gameObject);
+        }
+    }
+}
