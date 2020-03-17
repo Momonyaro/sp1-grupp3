c5528975f5c44768c9d6a74c0f31fc04d8b7adcb
Teodor Schulman <teodor.schulman@gmail.com>
2020-03-17 14:41:42 +0100

    :thumbsup:

---
+++ Assets/Scripts/Translator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Translator : MonoBehaviour
+{
+    public string swedishText;
+    public string englishText;
+
+    public Language language;
+    public Text _text;
+
+    private void Update()
+    {
+        if (OptionManager.GetIntIfExists("language") != int.MinValue)
+        {
+            language = (Language)OptionManager.GetIntIfExists("language");
+        }
+
+        if (language == Language.Swedish)
+        {
+            _text.text = swedishText;
+        }
+        if (language == Language.English)
+        {
+            _text.text = englishText;
+        }
+    }
+}
