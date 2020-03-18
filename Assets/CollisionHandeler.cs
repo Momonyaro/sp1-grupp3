d1b2e1ce0335c97eef9cec5a87f2269b5e436e39
b19stiha <b19stiha@users.noreply.github.com>
2020-03-18 12:19:21 +0100

    wip

---
+++ Assets/CollisionHandeler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandeler : MonoBehaviour
+{
+    public void KnockingBack(int i)
+    {
+        if(i == 1)
+        {
+            Debug.Log("i = " + i);
+        }
+        if(i == 2)
+        {
+            Debug.Log("i = " + i);
+
+        }
+        if (i == 3)
+        {
+            Debug.Log("i = " + i);
+
+        }
+        if (i == 4)
+        {
+            Debug.Log("i = " + i);
+
+        }
+    }
+}
